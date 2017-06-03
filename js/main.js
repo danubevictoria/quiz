a65b381afc27678673bd67d1cd21e0b599358913
Danube Phan <dphan@devonway.com>
2017-06-03 13:18:44 -0700

    updated results

--- js/main.js
+++ js/main.js
@@ -193,8 +193,9 @@ $('.value-btn').mousedown(function () {
 	var this_group = classArr[0];
 	// console.log(this_group);
 
-	// If button is already selected, de-select it when clicked
+	// If button is already selected, de-select it when clicked and subtract any previously added values to the total
 	// Otherwise, de-select any selected buttons in group and select the one just clicked
+	// And subtract deselected weighted value and add the newly selected weighted value to the total
 	if($(this).hasClass('active')) {
 		$(this).removeClass('active');
 		total -= (findPromptWeight(prompts, this_group) * findValueWeight(prompt_values, $(this).text()));
@@ -218,6 +219,30 @@ $('.value-btn').mousedown(function () {
 $('#submit-btn').click(function () {
 	// After clicking submit, add up the totals from answers
 	// For each group, find the value that is active
+	$('.results').removeClass('hide');
+	$('.results').addClass('show');
+	
+	if(total < 0) {
+		// document.getElementById('intro-bar').style.width = ((total / 60) * 100) + '%';
+		// console.log(document.getElementById('intro-bar').style.width);
+		// document.getElementById('intro-bar').innerHTML= ((total / 60) * 100) + '%';
+		document.getElementById('results').innerHTML = '<b>You are introverted!</b><br><br>\
+		Introverts are tricky to understand, since it’s so easy for us to assume that introversion is the same as being shy, when, in fact, introverts are simply people who find it tiring to be around other people.\n\
+<br><br>\
+I love this explanation of an introvert’s need to be alone:\n\
+<br><br>\
+For introverts, to be alone with our thoughts is as restorative as sleeping, as nourishing as eating.\n\n\
+<br><br>\
+Introverted people are known for thinking things through before they speak, enjoying small, close groups of friends and one-on-one time, needing time alone to recharge, and being upset by unexpected changes or last-minute surprises. Introverts are not necessarily shy and may not even avoid social situations, but they will definitely need some time alone or just with close friends or family after spending time in a big crowd.\
+		';
+	} else {
+		document.getElementById('results').innerHTML = '<b>You are extroverted!</b><br><br>\
+		On the opposite side of the coin, people who are extroverted are energized by people. They usually enjoy spending time with others, as this is how they recharge from time spent alone focusing or working hard.\
+<br><br>\
+I like how this extrovert explains the way he/she gains energy from being around other people:\
+<br><br>\
+When I am among people, I make eye contact, smile, maybe chat if there’s an opportunity (like being stuck in a long grocery store line). As an extrovert, that’s a small ‘ping’ of energy, a little positive moment in the day.';
+	}
 
 	// Hide the quiz after they submit their results
 	$('#quiz').addClass('hide');
@@ -230,4 +255,7 @@ $('#retake-btn').click(function () {
 	$('#quiz').removeClass('hide');
 	$('#submit-btn').removeClass('hide');
 	$('#retake-btn').addClass('hide');
+
+	$('.results').addClass('hide');
+	$('.results').removeClass('show');
 })
